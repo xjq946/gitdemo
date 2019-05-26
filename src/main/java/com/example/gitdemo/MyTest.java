9ed826db3853077d72bf15ab4dc5546167e5c54e
xjq946 <xjq111006@163.com>
2019-05-27 01:48:26 +0800

    update date

--- src/main/java/com/example/gitdemo/MyTest.java
+++ src/main/java/com/example/gitdemo/MyTest.java
@@ -1,11 +1,13 @@
 package com.example.gitdemo;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MyTest {
 
     public void test(){
         ArrayList<String> array=new ArrayList<>();
+        Date d=new Date();
         System.out.println("MyTest.test");
     }
 }
